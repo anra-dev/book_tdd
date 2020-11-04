9f3fef028b2868f3c26c4b2a4d121574274ff41b
Roman <anra@bk.ru>
2020-11-04 23:36:57 +0400

    Список умеет выводить 1 элемент. ФТ дописан до проверки 2х элементов.

--- functional_tests.py
+++ functional_tests.py
@@ -21,6 +21,7 @@ class NewVisitorTest(unittest.TestCase):
         # неотложных дел. Она решает оценить его домашнюю страницу
         self.browser.get('http://localhost:8000')
 
+
         # Она видит, что заголовок и шапка страницы говорят о списках
         # неотложных дел
         self.assertIn('To-Do', self.browser.title)
@@ -41,25 +42,31 @@ class NewVisitorTest(unittest.TestCase):
         # Когда она нажимает enter, страница обновляется, и теперь страница 
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(2)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Купить павлиньи перья' for row in rows),
-            "Новый элемент списка не появился в таблице"
-        )
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
         # (Эдит очень методична)
-        self.fail("Закончить тест!")
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
+
         # Страница снова обновляется, и теперь показывает оба элемента ее списка
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.assertIn('2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
+        
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что 
         # сайт сгенерировал для нее уникальный URL-адрес – об этом
         # выводится небольшой текст с объяснениями.
-
+        self.fail("Закончить тест!")
         # Она посещает этот URL-адрес – ее список по-прежнему там. 
         # Удовлетворенная, она снова ложится спать.
 if __name__ == '__main__':
