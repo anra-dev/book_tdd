f6e0ee3345d4aad05b2f48c0f76480465ed86e84
Roman <anra@bk.ru>
2020-11-06 23:09:52 +0400

    Переадресация после POST и показ всех элементов в шаблоне

--- lists/tests.py
+++ lists/tests.py
@@ -7,37 +7,58 @@ from lists.models import Item
 
 
 class HomePageTest(TestCase):
-	"""тест домашней страницы"""
+    """тест домашней страницы"""
 
-	def test_uses_home_temlates(self):
-		"""тест: используется домашний шаблон"""
-		response = self.client.get('/')
-		self.assertTemplateUsed(response, 'home.html')
+    def test_uses_home_temlates(self):
+        """тест: используется домашний шаблон"""
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-	def test_can_save_a_POST_request(self):
-		"""тест: может сохранить POST запрос"""
-		response = self.client.post('/', data={'item_text': 'A new list item'})
-		self.assertIn('A new list item', response.content.decode())
-		self.assertTemplateUsed(response, 'home.html')
+    def test_can_save_a_POST_request(self):
+        """тест: может сохранить POST запрос"""
+        self.client.post('/', data={'item_text': 'A new list item'})
 
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+        
+    def test_redirect_after_POST(self):
+        """тест: переадресует после post-запроса"""
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        """тест: сохраняет элемент, только когда нужно"""
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_items(self):
+        """тест: отображает все элементы списка"""
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
-	"""тест модели элемента списка"""
+    """тест модели элемента списка"""
 
-	def test_saving_and_retrieving_items(self):
-		"""тест сохранения и получения элементов списка"""
-		first_item = Item()
-		first_item.text = 'The first (ever) list item'
-		first_item.save()
+    def test_saving_and_retrieving_items(self):
+        """тест сохранения и получения элементов списка"""
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
 
-		second_item = Item()
-		second_item.text = 'Item the second'
-		second_item.save()
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
 
-		saved_item = Item.objects.all()
-		self.assertEqual(saved_item.count(), 2)
+        saved_item = Item.objects.all()
+        self.assertEqual(saved_item.count(), 2)
 
-		first_saved_item = saved_item[0]
-		second_saved_item = saved_item[1]
-		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-		self.assertEqual(second_saved_item.text, 'Item the second')
+        first_saved_item = saved_item[0]
+        second_saved_item = saved_item[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
