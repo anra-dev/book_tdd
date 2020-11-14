c235df7ecf8625f5580e379f2bc8dac70a942cda
Roman <anra@bk.ru>
2020-11-14 15:10:01 +0400

    Создали представление для нового списка и изменили шаблоны

--- lists/tests.py
+++ lists/tests.py
@@ -13,26 +13,7 @@ class HomePageTest(TestCase):
         """тест: используется домашний шаблон"""
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-
-    def test_can_save_a_POST_request(self):
-        """тест: может сохранить POST запрос"""
-        self.client.post('/', data={'item_text': 'A new list item'})
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-        
-    def test_redirect_after_POST(self):
-        """тест: переадресует после post-запроса"""
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/odin-edinstvennyi-spisok-v-mire/')
-
-    def test_only_saves_items_when_necessary(self):
-        """тест: сохраняет элемент, только когда нужно"""
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
+ 
 class ItemModelTest(TestCase):
     """тест модели элемента списка"""
 
@@ -53,7 +34,7 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_item[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
-
+        
 class ListViewTest(TestCase):
     """тест представления списка"""
 
@@ -72,5 +53,20 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
 
+class NewListTest(TestCase):
+    """тест нового списка"""
+
+    def test_can_save_a_POST_request(self):
+        """тест: может сохранить POST запрос"""
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+        
+    def test_redirect_after_POST(self):
+        """тест: переадресует после post-запроса"""
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/odin-edinstvennyi-spisok-v-mire/')
+        
 
 
--- lists/views.py
+++ lists/views.py
@@ -5,14 +5,15 @@ from lists.models import Item
 
 
 def home_page(request):
-	"""домашняя страница"""
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/lists/odin-edinstvennyi-spisok-v-mire/')
-	items = Item.objects.all()		
+	"""домашняя страница"""	
 	return render(request, 'home.html')
 
 def view_list(request):
 	"""новы список"""
 	items = Item.objects.all()		
 	return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+	"""новый список"""
+	Item.objects.create(text=request.POST['item_text'])
+	return redirect('/lists/odin-edinstvennyi-spisok-v-mire/')
