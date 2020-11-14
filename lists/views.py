9ca6925637064fab835d8f86f54426ad33e170ef
Roman <anra@bk.ru>
2020-11-15 00:23:53 +0400

    Новый URL + представления для добавления в существующие списки. ФТ проходит:-)

--- lists/views.py
+++ lists/views.py
@@ -8,13 +8,19 @@ def home_page(request):
 	"""домашняя страница"""	
 	return render(request, 'home.html')
 
-def view_list(request):
-	"""новы список"""
-	items = Item.objects.all()		
-	return render(request, 'list.html', {'items': items})
+def view_list(request, list_id):
+	"""представление списка"""
+	list_ = List.objects.get(id=list_id)		
+	return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
 	"""новый список"""
 	list_ = List.objects.create()
 	Item.objects.create(text=request.POST['item_text'], list=list_)
-	return redirect('/lists/odin-edinstvennyi-spisok-v-mire/')
+	return redirect(f'/lists/{list_.id}/')
+
+def add_item(request, list_id):
+	"""добавить элемент"""
+	list_ = List.objects.get(id=list_id)
+	Item.objects.create(text=request.POST['item_text'], list=list_)
+	return redirect(f'/lists/{list_.id}/')
