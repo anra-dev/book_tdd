fa9fd257c14714c6ecd0c368517cd60b464e2d74
Roman <anra@bk.ru>
2020-11-14 19:13:28 +0400

    Добавили модели для списков

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 
+class List(models.Model):
+	pass
+
 class Item(models.Model):
 	"""элемент списка"""
 	text = models.TextField(default='')
-
+	list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
