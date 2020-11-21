7b1d5a3e54072e1fd286daa9960b9b9fd1b09eda
Roman <anra@bk.ru>
2020-11-21 22:29:36 +0400

    Обеспечена валидация модели в представлении списка

--- lists/views.py
+++ lists/views.py
@@ -6,26 +6,32 @@ from lists.models import Item, List
 
 
 def home_page(request):
-	"""домашняя страница"""	
-	return render(request, 'home.html')
+    """домашняя страница""" 
+    return render(request, 'home.html')
 
 def view_list(request, list_id):
-	"""представление списка"""
-	list_ = List.objects.get(id=list_id)
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'], list=list_)
-		return redirect(f'/lists/{list_.id}/')
-	return render(request, 'list.html', {'list': list_})
+    """представление списка"""
+    list_ = List.objects.get(id=list_id)
+    error = None
+    if request.method == 'POST':
+        try:
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect(f'/lists/{list_.id}/')
+        except ValidationError:
+           error = "You can't have an empty list item" 
+    return render(request, 'list.html', {'list': list_, 'error': error})
 
 def new_list(request):
-	"""новый список"""
-	list_ = List.objects.create()
-	item = Item(text=request.POST['item_text'], list=list_)
-	try:
-		item.full_clean()
-		item.save()
-	except ValidationError:
-		list_.delete()
-		error = "You can't have an empty list item"
-		return render(request, 'home.html', {'error': error})
-	return redirect(f'/lists/{list_.id}/')
+    """новый список"""
+    list_ = List.objects.create()
+    item = Item(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {'error': error})
+    return redirect(f'/lists/{list_.id}/')
