c0115f5d7ed4a7135bb1466521cf71aa4f1924c8
Roman <anra@bk.ru>
2020-11-22 12:28:54 +0400

    Переименованы все id и name в элементах input элементов. По-прежнему не работает

--- lists/views.py
+++ lists/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.core.exceptions import ValidationError
-
+from lists.forms import ItemForm
 from lists.models import Item, List
 
 
 def home_page(request):
     """домашняя страница""" 
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'form': ItemForm()})
 
 def view_list(request, list_id):
     """представление списка"""
@@ -15,7 +15,7 @@ def view_list(request, list_id):
     error = None
     if request.method == 'POST':
         try:
-            item = Item(text=request.POST['item_text'], list=list_)
+            item = Item(text=request.POST['text'], list=list_)
             item.full_clean()
             item.save()
             return redirect(list_)
@@ -26,7 +26,7 @@ def view_list(request, list_id):
 def new_list(request):
     """новый список"""
     list_ = List.objects.create()
-    item = Item(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['text'], list=list_)
     try:
         item.full_clean()
         item.save()
