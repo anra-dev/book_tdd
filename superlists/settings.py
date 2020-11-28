8203f7a7a1b40d4e8f52961fc36df29602b7342f
Roman <anra@bk.ru>
2020-11-28 23:04:21 +0400

    Добавил настройки логирования и создал пустое приложение

--- superlists/settings.py
+++ superlists/settings.py
@@ -121,3 +121,21 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = '../static/'
+
+# Logging settings
+LOGGING = { 
+    'version': 1,
+    'disable_existing_loggers': False, 
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }, 
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'], 
+        },
+    },
+    'root': {'level': 'INFO'},
+}
