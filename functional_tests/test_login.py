e03229dbda33257bfb57d70ea91de0d867c51813
Roman <anra@bk.ru>
2020-12-08 23:33:47 +0400

    ФТ умеет управлять базой на сервере

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,10 +1,13 @@
+import re
+import poplib
+import os
+import time
 from django.core import mail
 from selenium.webdriver.common.keys import Keys
-import re
+
 
 from .base import FunctionalTest
 
-TEST_EMAIL = 'adith@example.com'
 SUBJECT = 'Your login link for Superlists'
 
 class LoginTest(FunctionalTest):
@@ -14,8 +17,13 @@ class LoginTest(FunctionalTest):
         # Эдит заходит на офигительный сайт суперсписков и впервые
         # замечает раздел "войти" в навигационной панели
         # Он говорит ей ввести свой адрес электронной почты, что она и делает
+        if self.staging_server:
+            test_email = 'superlists.f.test@gmail.com'
+        else:
+            test_email = 'edith@example.com'
+
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+        self.browser.find_element_by_name('email').send_keys(test_email)
         self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
 
         # Появляется сообщение, которое говорит, что ей на почту
@@ -26,15 +34,13 @@ class LoginTest(FunctionalTest):
         ))
 
         # Эдит проверяет свою почту и находит сообщение
-        email = mail.outbox[0]
-        self.assertIn(TEST_EMAIL, email.to)
-        self.assertEqual(email.subject, SUBJECT)
+        body = self.wait_for_email(test_email, SUBJECT)
 
         # Оно содержит ссылку на url-адрес
-        self.assertIn('Use this link to log in', email.body)
-        url_search = re.search(r'http://.+/.+$', email.body)
+        self.assertIn('Use this link to log in', body)
+        url_search = re.search(r'http://.+/.+$', body)
         if not url_search:
-            self.fail(f'Could not url in body:\n{email.body}')
+            self.fail(f'Could not url in body:\n{body}')
         url = url_search.group(0)
         self.assertIn(self.live_server_url, url)
 
@@ -42,13 +48,47 @@ class LoginTest(FunctionalTest):
         self.browser.get(url)
 
         # Она зарегистрирована в системе!
-        self.wait_to_be_logged_in(email=TEST_EMAIL)
+        self.wait_to_be_logged_in(email=test_email)
 
         # Тепрь она выходит из системы
         self.browser.find_element_by_link_text('Log out').click()
 
         # Она вышла из системы
-        self.wait_to_be_logged_out(email=TEST_EMAIL)
+        self.wait_to_be_logged_out(email=test_email)
+
+    def wait_for_email(self, test_email, subject):
+        """ожидать электронное сообщение"""
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(email.subject, subject)
+            return email.body
+
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('pop.gmail.com')
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.environ['GMAIL_PASSWORD'])
+            while time.time() - start < 60:
+                # получить 10 самых новых сообщений
+                count, _ = inbox.stat()
+                print(count)
+                for i in reversed(range(max(1, count - 10), count + 1)):
+                    print('getting msg', i)
+                    _, lines, __ = inbox.retr(i)
+                    #print(lines)
+                    lines = [l.decode('utf8') for l in lines]
+                    if f'Subject: {subject}' in lines:
+                        email_id = i
+                        body = '\n'.join(lines)
+                        return body
+                time.sleep(5)
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+            inbox.quit()
+
 
 
 
